a4bd949e76cba5be4c68897462fae74989941e96
vadim <vagapov.v@mail.ru>
2024-05-16 20:55:12 +0500

    4. Переопределение метода [#174890 #496323]

--- src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -3,8 +3,6 @@ package ru.job4j.inheritance;
 public class ReportUsage {
     public static void main(String[] args) {
         JSONReport report = new JSONReport();
-//        HtmlReport report = new HtmlReport();
-//        TextReport report = new TextReport();
         String text = report.generate("Report's name", "Report's body");
         System.out.println(text);
     }
