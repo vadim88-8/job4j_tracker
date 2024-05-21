59ded06677ba8239b9f743274e2334454d1ecc5d
vadim <vagapov.v@mail.ru>
2024-05-21 21:19:56 +0500

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book theABC = new Book("Азбука", 33);
+        Book theKamaSutra = new Book("Камасутра", 69);
+        Book javaTheCompleteReference = new Book("Java: The Complete Reference", 1345);
+        Book theCleanCode = new Book("Clean code", 464);
+        Book[] books = new Book[4];
+        books[0] = theABC;
+        books[1] = theKamaSutra;
+        books[2] = javaTheCompleteReference;
+        books[3] = theCleanCode;
+        for (Book book : books) {
+            System.out.println(book.getName() + " - " + book.getNumberOfPage());
+        }
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (Book book : books) {
+            if (book.getName().equals("Clean code")) {
+                System.out.println(book.getName() + " - " + book.getNumberOfPage());
+            }
+        }
+    }
+}
