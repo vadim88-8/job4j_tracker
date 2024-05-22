ec02e2d36d0f7f0ae915c24353f734981614f337
vadim <vagapov.v@mail.ru>
2024-05-22 21:04:40 +0500

    3. Массивы и модели. [#174892 #497207]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -19,7 +19,7 @@ public class Library {
         books[0] = books[3];
         books[3] = temp;
         for (Book book : books) {
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + " - " + book.getNumberOfPage());
             }
         }
