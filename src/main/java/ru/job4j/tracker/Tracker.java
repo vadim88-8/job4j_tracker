eabe250239133a367a52a8ea60e7bfc1aa3d1be4
vadim <vagapov.v@mail.ru>
2024-05-26 13:13:24 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -20,6 +22,29 @@ public class Tracker {
                 break;
             }
         }
+        return result;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            Item temp = items[i];
+            if (temp != null) {
+                result[size] = items[i];
+            }
+        }
+        result = Arrays.copyOf(items, size);
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && key.equals(items[i].getName())) {
+                result[size] = items[i];
+            }
+        }
+        result = Arrays.copyOf(items, size);
         return result;
     }
 }
